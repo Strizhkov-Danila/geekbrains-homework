ad3c978a740e577ce672a0e995df42ceef0ec407
Strizhkov <dany.strizh@gmail.com>
2021-01-30 22:20:27 +0300

    Exercise 1 complete!

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+extern int exi;
+static int si;
+
+void change ();
+
+int main () {
+	char ch1 = 75, ch2 = 'K';
+	short sh;
+	int i1 = 0, i2;
+	unsigned int ui = 1324;
+	float f;
+
+	exi = 35;
+	si = -32;
+	sh = -12354;
+	i2 = -1297;
+	f = 25.694;
+
+	void change();
+
+	return 0;
+}
+
+
+
+
+
+
---
+++ somefunc.cpp
@@ -0,0 +1,5 @@
+int exi;
+
+void change () {
+	exi = 132;
+}
