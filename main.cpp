56fa8058878177cb24e13ea8c225383ea81bd758
Strizhkov <dany.strizh@gmail.com>
2021-01-30 23:55:01 +0300

    Exercise 3 complete!

--- main.cpp
+++ main.cpp
@@ -18,14 +18,19 @@ int main () {
 	si = -32;
 	sh = -12354;
 	i2 = -1297;
-	f = 25.694;
+	f = 24.694;
 
 	void change();
 	
 	// Exerise 2
-	enum null_cross {Null = 89, Cross = 98};
-	
+	enum null_cross {Null = 79, Cross = 88};
 
+	// Exercise 3
+	char nac[3][3] = {{Null, Cross, Cross}, {Null, Null, Cross}, {Cross, Null, Cross}};
+	cout << nac[0][0] << " " << nac[0][1] << " " << nac[0][2] << endl;
+	cout << nac[1][0] << " " << nac[1][1] << " " << nac[1][2] << endl;
+	cout << nac[2][0] << " " << nac[2][1] << " " << nac[2][2] << endl;
+	
 	return 0;
 }
 
