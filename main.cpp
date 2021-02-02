2e73b7df016b2ed6bb8d833c539382121ab162d3
Strizhkov <dany.strizh@gmail.com>
2021-02-03 01:22:16 +0300

    Exercise 1 complete!

---
+++ main.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main (int argc, char** argv) {
+	
+	// Exercise 1
+	const int a = 25, b = 54, c = 194;
+	const double d = 43;
+	double result;
+	result = a * ( b + (c / d) );
+	cout << "result = " << result;
+
+
+
+
+	return 0;
+}
