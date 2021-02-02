7c7ffca78cbb315a3ebc5154ed15a44bcde8a1ff
Strizhkov <dany.strizh@gmail.com>
2021-02-03 02:40:38 +0300

    Exercise 4 complete!

--- main.cpp
+++ main.cpp
@@ -22,7 +22,19 @@ int main (int argc, char** argv) {
 	// Exercise 3
 	
 	result = e * ( f + (g / h) );
- 	cout << "result = " << result;
+ 	cout << "result = " << result << endl;
+	
+	//Exercise 4
+	int arr[3][3][3] =  {
+			    {{11, 12, 13}, {14, 15, 16}, {17, 18, 19}},
+			    {{10, 11, 12}, {13, 14, 15}, {16, 17, 18}},
+			    {{19, 20, 21}, {22, 23, 24}, {25, 26, 27}}
+			    };
+	int* pointer = arr[0][0];
+	// cout << "&arr[0][0][0]" << pointer << endl;
+	// cout << "arr[0][0][0] = " << *pointer << endl;
+	// cout << "&arr[1][1][1] = " << (pointer + 3 + 3 + 3 + 3 + 1) << endl;
+	cout << "arr[1][1][1] = " << *(pointer + 3 + 3 + 3 + 3 + 1) << endl;
 
 	return 0;
 }
